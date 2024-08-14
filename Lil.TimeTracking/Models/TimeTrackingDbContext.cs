98b4edd482396fa8989713b3671e86a0cf6da752
iman <dev.imansabet@gmail.com>
2024-08-14 20:29:22 +0330

    Add project files.

---
+++ Lil.TimeTracking/Models/TimeTrackingDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Lil.TimeTracking.Models;
+
+public  class TimeTrackingDbContext : DbContext
+{
+
+
+    public TimeTrackingDbContext()
+    {}
+     public TimeTrackingDbContext(DbContextOptions options) : base(options)
+    {}
+
+    public DbSet<Employee> Employees { get; set; } 
+    public DbSet<Project> Projects { get; set; } 
+    public DbSet<TimeEntry> TimeEntries { get; set; } 
+}
