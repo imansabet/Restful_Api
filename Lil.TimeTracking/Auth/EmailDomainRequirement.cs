0533fa5108c219a667705f810b882c02aaa76eaf
iman <dev.imansabet@gmail.com>
2024-08-16 14:20:29 +0330

    defining security policies

---
+++ Lil.TimeTracking/Auth/EmailDomainRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Lil.TimeTracking.Auth;
+
+public class EmailDomainRequirement : IAuthorizationRequirement
+{
+    public EmailDomainRequirement(string domain)
+    {
+        Domain = domain;
+    }
+
+    public string Domain { get; set; }
+}
