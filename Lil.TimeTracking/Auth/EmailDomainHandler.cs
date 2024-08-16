97538390184736423a3ca03e88012a56d6e101e8
iman <dev.imansabet@gmail.com>
2024-08-16 14:29:02 +0330

    creating a security policy for write operation

---
+++ Lil.TimeTracking/Auth/EmailDomainHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace Lil.TimeTracking.Auth;
+
+public class EmailDomainHandler : AuthorizationHandler<EmailDomainRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmailDomainRequirement requirement)
+    {
+        var emailClaim = context.User.FindFirst(
+            c => c.Type == ClaimTypes.Email);
+
+        if (emailClaim is null)
+        {
+            return Task.CompletedTask;
+        }
+
+        if (emailClaim.Value.EndsWith(requirement.Domain,
+            StringComparison.InvariantCultureIgnoreCase))
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+
+    }
+}
